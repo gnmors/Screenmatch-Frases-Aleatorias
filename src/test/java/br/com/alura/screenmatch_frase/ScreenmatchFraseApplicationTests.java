575d7f13109fd7f547669a678be4eba3f07c6f5d
Giovanni Morais <giovannimo1@yahoo.com>
2024-11-01 17:38:12 -0300

    Initial Commit

---
+++ src/test/java/br/com/alura/screenmatch_frase/ScreenmatchFraseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch_frase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchFraseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
